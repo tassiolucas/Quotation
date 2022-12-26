88e8af99eaa8fc5e0f72f0aae6912864b6f0a040
Tássio Lucas <tassiolucas@live.com>
2022-12-25 23:59:59 -0300

    - Remove annotations not used.

--- app/src/main/java/com/quotation/presentation/ui/QuotationFragment.kt
+++ app/src/main/java/com/quotation/presentation/ui/QuotationFragment.kt
@@ -1,12 +1,10 @@
 package com.quotation.presentation.ui
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.quotation.R
@@ -31,7 +29,6 @@ class QuotationFragment : Fragment() {
         binding = this
     }.root
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -51,7 +48,11 @@ class QuotationFragment : Fragment() {
         }
 
         viewModel.onConnectionFailedEvent.observe(viewLifecycleOwner) {
-            Toast.makeText(requireContext(), getString(R.string.connection_failure_message, it), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.connection_failure_message, it),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
